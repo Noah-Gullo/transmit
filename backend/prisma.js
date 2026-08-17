8a635742acc985d842e7da7b4b1c4f3a8a5597fb
Noah Gullo <noahgulloingram@gmail.com>
2026-08-17 06:33:10 -0700

    fix: add prisma db log

--- backend/prisma.js
+++ backend/prisma.js
@@ -1,6 +1,13 @@
 const { PrismaPg } = require("@prisma/adapter-pg");
 const { PrismaClient } = require("./generated/prisma");
 
+// Temporary debugging
+const dbUrl = new URL(process.env.DATABASE_URL);
+
+console.log("DB host:", dbUrl.hostname);
+console.log("DB port:", dbUrl.port);
+console.log("DB name:", dbUrl.pathname);
+
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
