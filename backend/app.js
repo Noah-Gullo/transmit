5a20f0639e1d617dfe1c19fd5bfe4fcc8eca071b
Noah Gullo <noahgulloingram@gmail.com>
2026-08-17 06:44:29 -0700

    fix: port env port

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const passport = require("./passport");
 const indexRouter = require("./routes/indexRouter");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("trust proxy", 1)
 
